1f45afcfce16525b54d72ce8b2e2e7cb87ade6ed
minimish1 <minimish007@gmail.com>
2025-11-14 11:51:39 +0000

    Added PC.sv

--- repo/rtl/PC.sv
+++ repo/rtl/PC.sv
@@ -4,18 +4,22 @@ module PC #(
     input logic clk,
     input logic rst,
     input logic PCsrc,
-    input logic immOp,
+    input logic [WIDTH-1:0] immOp,
     output logic [WIDTH-1:0] pc
 );
 
 logic [WIDTH-1:0] branch_PC;
 logic [WIDTH-1:0] inc_PC;
+logic [WIDTH-1:0] next_PC;
 
-always_ff @(posedge clk)
-    if (rst) pc <= {WIDTH{1'b0}};
+assign inc_PC = pc + WIDTH'd4; 
+assign branch_PC = pc + immOp;
+assign next_PC = PCsrc ? branch_PC : inc_PC;
+
+always_ff @(posedge clk or posedge rst) begin
+    if (rst) 
+        pc <= {WIDTH{1'b0}};
     else
-        assign inc_PC = pc + {{WIDTH-3{1'b0}}, 3'b100}; // pc + 4
-        assign branch_PC = pc + immOp;
-        if (PCsrc) pc <= branch_PC;
-        else pc <= inc_PC;
+        pc <= next_PC;
+    end
 endmodule
