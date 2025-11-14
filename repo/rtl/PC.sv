72ac2d6393fea44e3a47138ead1f0b5d7ad8f96f
minimish1 <minimish007@gmail.com>
2025-11-14 11:41:19 +0000

    Added PC.sv

--- repo/rtl/PC.sv
+++ repo/rtl/PC.sv
@@ -14,7 +14,7 @@ logic [WIDTH-1:0] inc_PC;
 always_ff @(posedge clk)
     if (rst) pc <= {WIDTH{1'b0}};
     else
-        assign inc_PC = pc + {{WIDTH-3{1'b0}}, 2'b100}; // pc + 4
+        assign inc_PC = pc + {{WIDTH-3{1'b0}}, 3'b100}; // pc + 4
         assign branch_PC = pc + immOp;
         if (PCsrc) pc <= branch_PC;
         else pc <= inc_PC;
