039bd08084c1435457964ca3d10efedeb6285180
Sicovo <sicovo@hotmail.com>
2025-11-24 14:50:08 +0000

    ALU Updated (3bits ALUControl)

--- repo/rtl/alu.sv
+++ repo/rtl/alu.sv
@@ -1,38 +1,23 @@
-module alu(
-
-    input logic [31:0] alu_op1,  // first operand for the ALU
-    input logic [31:0] alu_op2,  // second operand for the ALU
-    input logic        alu_ctrl, // ALU control signal: selects operation
-                                 // 0 -> addition (for addi)
-                                 // 1 -> subtraction (for bne)
-
-    output logic [31:0] alu_out, // ALU output result
-    output logic        eq       // equality flag: 1 if alu_op1 == alu_op2
+module alu (
+    input logic [31:0] SrcA,
+    input logic [31:0] SrcB,
+    input logic [2:0] ALUControl,
+    output logic [31:0] ALUResult,
+    output logic Zero
 );
 
-// internal signals to hold intermediate results
-logic [31:0] equality; // difference between operands, used for comparison
-logic [31:0] sum;      // sum of operands, used for addition
+    always_comb begin
+        case (ALUControl)
+            3'b000: ALUResult = SrcA + SrcB; // add
+            3'b001: ALUResult = SrcA - SrcB; // sub
+            3'b010: ALUResult = SrcA & SrcB; // and
+            3'b011: ALUResult = SrcA | SrcB; // or
+            3'b101: ALUResult = ($signed(SrcA) < $signed(SrcB)) ? 32'b1:32'b0;
+            default: ALUResult = 32'b0;
 
-// combinational logic block (no clock)
-always_comb begin
+        endcase
+    end
+    
+    assign Zero = (ALUResult == 32'b0);
 
-    // compute subtraction for branch comparison
-    equality = alu_op1 - alu_op2;
-    // compute addition for arithmetic instructions
-    sum = alu_op1 + alu_op2;
-
-    // select ALU output based on control signal
-    if(alu_ctrl == 1'b0)
-        alu_out = sum;             // alu_ctrl=0 -> addition (addi)
-    else
-        alu_out = equality;        // alu_ctrl=1 → subtraction (bne)
-
-    // set equality flag: 1 if operands are equal, 0 otherwise
-    if(equality == 32'b0)
-        eq = 1;
-    else
-        eq = 0;
-
-end
 endmodule
